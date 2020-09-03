1e892cc5e23f0e654aaed1d29de4c909753b64b4
Kourouma Aboubacar Sidiki <boubasid2000@yahoo.com>
2020-09-03 15:03:09 +0000

    adding a css in vote control

--- app/Answer.php
+++ app/Answer.php
@@ -24,4 +24,10 @@ class Answer extends Model
         });
 
     }
+    public function getCreatedDateAttribute(){
+
+        return $this->created_at->diffForHumans();
+    }
+
+
 }
---
+++ resources/assets/js/fontawesome.js
@@ -0,0 +1,7 @@
+import fontawesome from '@fortawesome/fontawesome/index';
+import faCareUp from '@fortawesome/fontawesome-free-solid/faCaretUp';
+import faCarDown from '@fortawesome/fontawesome-free-solid/faCaretDown';
+import faStart from '@fortawesome/fontawesome-free-solid/faStar';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+
+fontawesome.library.add([faCarDown,faCareUp,faCheck,faStart]);
